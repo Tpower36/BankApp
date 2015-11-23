f693cac9a9f1cc8847b9cb99f2375c83b90dd995
Thierry Hubmann <thierry.hubmann@he-arc.ch>
2015-11-23 13:05:45 +0100

    Correction d'erreurs dans l'internationalisation qui est désormais fonctionelle

---
+++ src/java/ch/hearc/ig/odi/customeraccount/managedbeans/LocaleBean.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.managedbeans;
+
+import java.io.Serializable;
+import java.util.Locale;
+import java.util.Map;
+import java.util.logging.Level;
+import javax.faces.context.FacesContext;
+import org.jboss.weld.annotated.enhanced.EnhancedAnnotated;
+import org.jboss.weld.bean.AbstractBean;
+
+/**
+ *
+ * @author thierry.hubmann
+ */
+public class LocaleBean {
+
+    private FacesContext ctx = FacesContext.getCurrentInstance();
+    private Locale locale = ctx.getViewRoot().getLocale();
+
+    public Locale getLocale() {
+       
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+       
+        this.locale = locale;
+    }
+
+    public String getLanguage() {
+        Locale newlocale = null;
+       
+        String lang = locale.getLanguage();
+        Map map = ctx.getExternalContext().getSessionMap();
+        if (map.containsKey("locale")) {
+            newlocale = (Locale) map.get("locale");
+        }
+        if (!(newlocale == null)) {
+            String newlang = newlocale.getLanguage();
+            if (!newlang.equals(lang)) {
+                return newlang;
+            }
+        }
+        return lang;
+    }
+
+    public void setLanguage(String language) {      
+        locale = new Locale(language);
+        ctx.getViewRoot().setLocale(locale);
+    }
+
+
+    
+    
+
+   
+}
