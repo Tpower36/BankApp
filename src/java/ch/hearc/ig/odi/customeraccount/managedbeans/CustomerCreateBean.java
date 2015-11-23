ac36e7d07b981298f1811ce3ff3e8c1ba463aeaa
Thierry Hubmann <thierry.hubmann@he-arc.ch>
2015-11-23 21:30:52 +0100

    Mise en fonction de la navigation statique entre la liste des clients et l'ajout de client. Création de headers sur la liste des clients. Modifications mineures sur le formulaire d'ajout d'un client.

---
+++ src/java/ch/hearc/ig/odi/customeraccount/managedbeans/CustomerCreateBean.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.hearc.ig.odi.customeraccount.managedbeans;
+
+import ch.hearc.ig.odi.customeraccount.services.Services;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author thierry.hubmann
+ */
+public class CustomerCreateBean {
+
+    @Inject Services services;
+    
+    private int number;
+    private String firstname;
+    private String lastname;
+    /**
+     * Creates a new instance of CustomerCreateBean
+     */
+    public CustomerCreateBean() {
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    
+    
+}
